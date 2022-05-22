b6e06a42fcaad3e6fb30f89179c441912994b8ce
louisthms <78725905+louisthms@users.noreply.github.com>
2022-05-22 18:39:02 +0200

    add js function

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,8 @@ $(function () {
             $('.navbar').removeClass('active');
         }
     });
+    $('a[href*=#]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top-80}, 500, 'linear');
+      });
 });
