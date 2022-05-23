c3544557e0c4a6e7772727f811acfa32210c5a04
louisthms <78725905+louisthms@users.noreply.github.com>
2022-05-23 15:18:46 +0200

    add competition page

--- competition.php
+++ competition.php
@@ -37,7 +37,152 @@
     </div>
 </nav>
 
+<!-- Section -->
+<div class="container my-5 py-5">
+<h3 class="text-center text-white my-5 py-5">Les Sports de compétition</h3>
+<!--Carousel Wrapper-->
+<div id="multi-item-example" class="carousel slide carousel-multi-item" data-ride="carousel">
 
+
+<!-- Contrôles -->
+<a class="carousel-control-prev" href="#multi-item-example" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Précédent</span>
+        </a>
+        <a class="carousel-control-next" href="#multi-item-example" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Suivant</span>
+        </a>
+
+    <!--Slides-->
+    <div class="carousel-inner" role="listbox">
+
+      <!--First slide-->
+
+        <div class="carousel-item active">
+            <div class= "row">
+            <div class="col-md-4">
+
+            </div>
+            <div class="col-md-4">
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="img/basket.jpg"
+                        alt="Card image cap">
+                        <div class="card-body text-center">
+                                <h4 class="card-title">Basketball</h4>
+                                <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the
+                                card's content.</p>
+                                <a class="btn btn-primary text-white">Sélectionner</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <div class= "row">
+                <div class="col-md-4">
+                    
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                    <img class="card-img-top" src="img/football.jpg"
+                        alt="Card image cap">
+                        <div class="card-body text-center">
+                            <h4 class="card-title">Football</h4>
+                            <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                            <a class="btn btn-primary text-white">Sélectionner</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <div class= "row">
+                <div class="col-md-4">
+                    
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                    <img class="card-img-top" src="img/rugby.jpg"
+                        alt="Card image cap">
+                        <div class="card-body text-center">
+                            <h4 class="card-title">Rugby</h4>
+                            <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                            <a class="btn btn-primary text-white">Sélectionner</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <div class= "row">
+                <div class="col-md-4">
+                    
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                    <img class="card-img-top" src="img/tennis.jpg"
+                        alt="Card image cap">
+                        <div class="card-body text-center">
+                            <h4 class="card-title">Tennis</h4>
+                            <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                            <a class="btn btn-primary text-white">Sélectionner</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <div class= "row">
+                <div class="col-md-4">
+                    
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                    <img class="card-img-top" src="img/natation.jpg"
+                        alt="Card image cap">
+                        <div class="card-body text-center">
+                            <h4 class="card-title">Natation</h4>
+                            <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                            <a class="btn btn-primary text-white">Sélectionner</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <div class= "row">
+                <div class="col-md-4">
+                    
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                    <img class="card-img-top" src="img/plongeon.jpg"
+                        alt="Card image cap">
+                        <div class="card-body text-center">
+                            <h4 class="card-title">Plongeon</h4>
+                            <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                            <a class="btn btn-primary text-white">Sélectionner</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+</div>
 
 <!-- Footer-->
 <footer id="footer" class="bg-white">
