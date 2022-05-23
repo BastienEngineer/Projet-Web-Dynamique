6d25003f8d85fa9e9422033bf94563337570f9bc
louisthms <78725905+louisthms@users.noreply.github.com>
2022-05-23 12:38:38 +0200

    add activites

--- activites.php
+++ activites.php
@@ -37,7 +37,141 @@
     </div>
 </nav>
 
+<!-- Section -->
+<div class="container my-5 py-5">
 
+<!--Carousel Wrapper-->
+<div id="multi-item-example" class="carousel slide carousel-multi-item" data-ride="carousel">
+
+
+<!-- Contrôles -->
+<a class="carousel-control-prev" href="#multi-item-example" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Précédent</span>
+        </a>
+        <a class="carousel-control-next" href="#multi-item-example" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Suivant</span>
+        </a>
+
+    <!--Indicators-->
+    <ol class="carousel-indicators">
+      <li data-target="#multi-item-example" data-slide-to="0" class="active"></li>
+      <li data-target="#multi-item-example" data-slide-to="1"></li>
+      <li data-target="#multi-item-example" data-slide-to="2"></li>
+    </ol>
+    <!--/.Indicators-->
+
+    <!--Slides-->
+    <div class="carousel-inner" role="listbox">
+
+      <!--First slide-->
+
+        <div class="carousel-item active">
+            <div class= "row">
+            <div class="col-md-4">
+
+            </div>
+            <div class="col-md-4">
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
+                        alt="Card image cap">
+                        <div class="card-body">
+                                <h4 class="card-title">Musculation</h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's content.</p>
+                                <a class="btn btn-primary">Button</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <div class= "row">
+                <div class="col-md-4">
+                    
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                    <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
+                        alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">Fitness</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                            <a class="btn btn-primary">Button</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <div class= "row">
+                <div class="col-md-4">
+                    
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                    <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg"
+                        alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">Biking</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                            <a class="btn btn-primary">Button</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <div class= "row">
+                <div class="col-md-4">
+                    
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                    <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg"
+                        alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title"> Cardio-Training</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                            <a class="btn btn-primary">Button</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <div class= "row">
+                <div class="col-md-4">
+                    
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                    <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg"
+                        alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">Cours Collectifs</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                            <a class="btn btn-primary">Button</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+      <!--/.First slide-->
+
+</div>
 
 <!-- Footer-->
 <footer id="footer" class="bg-white">
