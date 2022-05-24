bb8fa97d438450c8c7bf1c8b3c085dca7d423a67
louisthms <78725905+louisthms@users.noreply.github.com>
2022-05-24 19:09:04 +0200

    update style login form

--- clientCreate.php
+++ clientCreate.php
@@ -1,64 +1,81 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Projet Piscine</title>
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Creation d un client</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <script src="js/scripts.js"></script>
 </head>
 <body>
+<div class="container my-5 py-5">
+<div class="row h-100 justify-content-center align-items-center">
     <form action="AjoutClient.php" method="post">
-        <table border="1">
+        <table class="table">
         <tr>
             <td colspan="2" align="center">
-            Creation d un client
+            <h3 class="text-white font-weight-light">Creation d un client</h3>
             </td>
         </tr>        
         <tr>
             <td>Nom</td>
-            <td><input type="text" name="nom"></td>
+            <td><input type="text" class="form-control" name="nom"></td>
         </tr>
         <tr>
             <td>Prenom</td>
-            <td><input type="text" name="prenom"></td>
+            <td><input type="text" class="form-control" name="prenom"></td>
         </tr>
         <tr>
             <td>Mail</td> 
-            <td><input type="email" name="courrier"></td> 
+            <td><input type="email" class="form-control" name="courrier"></td> 
         </tr>
         <tr>
             <td>Mot de passe</td>
-            <td><input type="password" name="mdp"></td>
+            <td><input type="password" class="form-control" name="mdp"></td>
         </tr>
         <tr>
             <td>Adresse</td>
-            <td><input type="text" name="adresse"></td>
+            <td><input type="text" class="form-control" name="adresse"></td>
         </tr>
         <tr>
             <td>Ville</td>
-            <td><input type="text" name="ville"></td>
+            <td><input type="text" class="form-control" name="ville"></td>
         </tr>
         <tr>
             <td>Code Postal</td>
-            <td><input type="text" pattern="[0-9]{5}" name="code"></td>
+            <td><input type="text" class="form-control" pattern="[0-9]{5}" name="code"></td>
         </tr>
         <tr>
             <td>Pays</td>
-            <td><input type="text" name="pays"></td>
+            <td><input type="text" class="form-control" name="pays"></td>
         </tr>
         <tr>
             <td>Telephone</td>
-            <td><input type="text" pattern="[0-9]{10}" name="tel"></td>
+            <td><input type="text" class="form-control" pattern="[0-9]{10}" name="tel"></td>
         </tr>
         <tr>
             <td>Carte Etudiante</td>
-            <td><input type="text" name="carte"></td>
+            <td><input type="text" class="form-control" name="carte"></td>
         </tr>
         <tr>
         <td colspan="2" align="center">
-        <input type="submit" value="Creer votre compte" name="creation">
+        <input type="submit" class="btn btn-outline-light btn-lg" value="Creer votre compte" name="creation">
         </td>
         </tr>
         </table>
     </form>
+</div>
+</div>
 </body>
 </html>
--- connexionB.php
+++ connexionB.php
@@ -1,14 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Projet Piscine</title>
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Client</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <script src="js/scripts.js"></script>
 </head>
 <body>
-    <form action="clientChoice.php" method="post">  
-        <input type="submit" value="Se connecter" name="login">  
-        <input type="submit" value="Creer un compte" name="login">  
-    </form>
+
+<div class="text-center text-white my-5 py-5">
+    <div class="container-fluid">
+        <div class="my-5 py-5">
+            <div class="row justify-content-center align-items-center h-100">
+                <form action="clientChoice.php" method="post">
+                    <input type="submit" class="btn btn-outline-light btn-lg" value="Se connecter" name="login">
+                    <input type="submit" class="btn btn-outline-light btn-lg" value="Creer un compte" name="login">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
