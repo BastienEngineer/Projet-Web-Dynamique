a30a5e1435fa8e332232075849ddf5bbd83e587f
JulienYU-2022 <shihao.yu1998@gmail.com>
2022-05-25 02:22:58 +0200

    email

--- email.php
+++ email.php
@@ -1,13 +1,14 @@
 <?php
 
+
 if(isset($_POST['valide']))
 {
-    $to      = 'shihao@outlook.fr';
-    $subject = 'Paiement réussir';
-    $message = 'votre paiement est réussir';
-    $headers = 'From: omnessports@gmail.com' . "\r\n" .
-
-    mail($to, $subject, $message, $headers);
+    $to = 'huaitaoy@gmail.com';
+    $subject = 'Paiement réussit';
+    $message = 'votre paiement est réussit';
+    $headers = 'From: omnessports@gmail.com';
+    
+    mail($to,$subject,$message,$headers);
 
     echo 'Email Sent.';
 }
