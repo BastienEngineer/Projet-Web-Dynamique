27c07a91a0496dfc287a6ab17c2c609de35f5dbb
JulienYU-2022 <shihao.yu1998@gmail.com>
2022-05-25 02:46:47 +0200

    envoye email

--- email.php
+++ email.php
@@ -1,16 +1,19 @@
 <?php
-
-
+session_start();
+$nom = $_POST['nom'];
+$getemail="SELECT Mail FROM client WHERE Nom='$nom'";
 if(isset($_POST['valide']))
 {
-    $to = 'huaitaoy@gmail.com';
+    $to = $getemail;
     $subject = 'Paiement réussit';
     $message = 'votre paiement est réussit';
     $headers = 'From: omnessports@gmail.com';
     
-    mail($to,$subject,$message,$headers);
-
-    echo 'Email Sent.';
+    if (mail ($to, $subject, $body, $from)) {
+            echo '<p>votre email a envoyé </p>';
+        } else {
+            echo '<p>votre email n a pas encore envoyé </p>';
+        }
 }
 
 ?>
