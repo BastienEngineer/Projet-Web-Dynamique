2de7bfe54272b443526c0b77b38fcdc52ac29187
kleytar <btxkleytar1002@gmail.com>
2022-05-26 22:49:30 +0200

    update consultation

--- coach.php
+++ coach.php
@@ -24,7 +24,8 @@ session_start();
 $nom = isset($_POST["nom"])? $_POST["nom"] : "";
 $prenom = isset($_POST["prenom"])? $_POST["prenom"] : "";
 $mail = isset($_POST["courrier"])? $_POST["courrier"] : "";
-
+$nomClient=0;
+$prenomClient="";
 if (isset($_SESSION['cID'])) {
     $nom=$_SESSION['Nom'];
     $prenom=$_SESSION['Prenom'];
@@ -65,6 +66,46 @@ else
     }
     ?>
     <a class="btn btn-outline-light btn-lg mt-5" href="deconnexion.php">Deconnexion</a>
+    </div>
+    </div>
+    </div>
+    </div>
+    <div class="container my-5 py-5">
+    <div class="border border-light py-5" style="border-radius: 33px;">
+    <h2 class="text-white text-center lead">Consultations</h2>
+    <div class="row justify-content-center align-items-center">
+    <div class="col-lg-4 col-md-6 my-4 py-4 text-center">
+    <?php 
+    $sess=$_SESSION['cID'];
+    $r=mysqli_query($db_handle, "SELECT * FROM rdv WHERE c_id=$sess");
+    if (mysqli_num_rows($r)) {
+    echo "<table border=\"1\">";
+    echo "<tr>";
+    echo "<th>" . "Jour" . "</th>";
+    echo "<th>" . "Horaire" . "</th>";
+    echo "<th>" . "Client" . "</th>";
+    echo "</tr>";
+    while ($data = mysqli_fetch_assoc($r)) {
+        $nomClient=$data['clientID'];
+        $r1=mysqli_query($db_handle, "SELECT Prenom FROM client WHERE mID=$nomClient");
+        if (mysqli_num_rows($r)) {
+        while ($data1 = mysqli_fetch_assoc($r1)) {
+            $prenomClient=$data1['Prenom'];
+        }
+        echo "<tr>";
+        echo "<td>" . $data['jour'] . "</td>";
+        echo "<td>" . $data['horaire'] . "</td>";
+        echo "<td>" .  $prenomClient . "</td>";
+        echo "</tr>";
+        }
+    }
+    echo "</table>";
+    }
+    else
+    {
+        echo "Aucune consultation";
+    }
+    ?>
     </div>
     </div>
     </div>
