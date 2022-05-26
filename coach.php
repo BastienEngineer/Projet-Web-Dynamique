de7c729dcc27217a3e25737ef70ea9238a0b2f14
louisthms <78725905+louisthms@users.noreply.github.com>
2022-05-26 10:27:34 +0200

    update design coach + modif button retour

--- coach.php
+++ coach.php
@@ -4,6 +4,12 @@ $nom = isset($_POST["nom"])? $_POST["nom"] : "";
 $prenom = isset($_POST["prenom"])? $_POST["prenom"] : "";
 $mail = isset($_POST["courrier"])? $_POST["courrier"] : "";
 
+if (isset($_SESSION['cID'])) {
+    $nom=$_SESSION['Nom'];
+    $prenom=$_SESSION['Prenom'];
+    $mail=$_SESSION['Mail'];
+} 
+
 $erreur = "";
 if ($nom == "") {
     $erreur .= "Le champ nom est vide. <br>";
@@ -18,46 +24,52 @@ if ($erreur != "") {
     echo "Erreur: <br>" . $erreur;
     header("Location:connexionC.php"); 
 }
-if (isset($_POST["coach"])) {
-    $database = "omnes";
-    //identifier votre serveur (localhost), utlisateur (root), mot de passe ("")
-    $db_handle = mysqli_connect('localhost', 'root', '');
-    $db_found = mysqli_select_db($db_handle, $database);
-    if ($db_found) {
-    $result =mysqli_query($db_handle, "SELECT * FROM coach WHERE Nom='$_POST[nom]' AND Prenom='$_POST[prenom]' AND Mail='$_POST[courrier]' ORDER BY cID ");
-    if (mysqli_num_rows($result)) {
-    echo "<table border=\"1\">";
-    echo "<tr>";
-    echo "<th>" . "ID" . "</th>";
-    echo "<th>" . "Nom" . "</th>";
-    echo "<th>" . "Prenom" . "</th>";
-    echo "</tr>";
-    while ($data = mysqli_fetch_assoc($result)) {
-    echo "<tr>";
-    echo "<td>" . $data['cID'] . "</td>";
-    echo "<td>" . $data['Nom'] . "</td>";
-    echo "<td>" . $data['Prenom'] . "</td>";
-    echo "</tr>";
-    $_SESSION['cID']=$data['cID'];
-    $e=$_SESSION['cID'];
+else
+{
+    if (isset($_POST["coach"])) {
+        $database = "omnes";
+        //identifier votre serveur (localhost), utlisateur (root), mot de passe ("")
+        $db_handle = mysqli_connect('localhost', 'root', '');
+        $db_found = mysqli_select_db($db_handle, $database);
+        if ($db_found) {
+        $result =mysqli_query($db_handle, "SELECT * FROM coach WHERE Nom='$_POST[nom]' AND Prenom='$_POST[prenom]' AND Mail='$_POST[courrier]' ORDER BY cID ");
+        if (mysqli_num_rows($result)) {
+        echo "<table border=\"1\">";
+        echo "<tr>";
+        echo "<th>" . "ID" . "</th>";
+        echo "<th>" . "Nom" . "</th>";
+        echo "<th>" . "Prenom" . "</th>";
+        echo "</tr>";
+        while ($data = mysqli_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>" . $data['cID'] . "</td>";
+        echo "<td>" . $data['Nom'] . "</td>";
+        echo "<td>" . $data['Prenom'] . "</td>";
+        echo "</tr>";
+        $_SESSION['cID']=$data['cID'];
+        $e=$_SESSION['cID'];
+        $_SESSION['Nom']=$data['Nom'];
+        $_SESSION['Prenom']=$data['Prenom'];
+        $_SESSION['Mail']=$data['Mail'];
+        }
+        echo "</table>";
+        $res=mysqli_query($db_handle, "SELECT mID,Prenom FROM client");
+        while ($data = mysqli_fetch_assoc($res)) {
+            $d=$data['mID'];
+            echo "<a href='communiquer.php?mID=$d'><br>" . $data['Prenom'] . "</br>";
+        }
+        }
+        else
+        {
+            header("Location:connexionC.php");
+        }
+        }
+        else{
+            echo "<br>Database not found";
+        }
+        //fermer la connexion
+        mysqli_close($db_handle);
     }
-    echo "</table>";
-    $res=mysqli_query($db_handle, "SELECT mID,Prenom FROM client");
-    while ($data = mysqli_fetch_assoc($res)) {
-        $d=$data['mID'];
-        echo "<a href='communiquer.php?mID=$d'><br>" . $data['Prenom'] . "</br>";
-    }
-    }
-    else
-    {
-        header("Location:connexionC.php");
-    }
-    }
-    else{
-        echo "<br>Database not found";
-    }
-    //fermer la connexion
-    mysqli_close($db_handle);
 }  
 ?>
 
--- communiquer.php
+++ communiquer.php
@@ -14,7 +14,7 @@ if ($db_found) {
         $sql="INSERT INTO echange(msgID,sms,dest,emet)
         VALUES($i,'$m',$d,$e)";
         $result =mysqli_query($db_handle, $sql);
-        echo "Message Envoye";
+        echo "<script type='text/javascript'>alert('Message envoyé');</script>";
     }
 }
 else
@@ -26,33 +26,63 @@ else
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projet Piscine</title>
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <script src="js/scripts.js"></script>
 </head>
 <body>
-    <form method="post" action="">
-    <textarea name="message"></textarea>
-    <input type="submit" name="send">
-    </form>
+<div class="container d-flex justify-content-center">
+    <div class="card mt-5" style="width:50%;">
+        <form method="post" action="">
+            <div class="d-flex flex-row justify-content-between p-3">
+                <i class="fa fa-chevron-left"></i>
+                <p class="lead font-weight-bold">Chat Room</p>
+                <i class="fa fa-times"></i>
+            </div>
+                <div class="form-group px-3">
+                    <textarea name="message" class="form-control" rows="5" placeholder="Saisir votre message..."></textarea>
+                </div>
+                <div class="col my-4 py-4 text-center">
+                    <?php echo "<a class='btn btn-outline-dark' href='coach.php'>Retour</a>"; ?>
+                    <input class="btn btn-outline-dark" type="submit" name="send">
+                </div>
+        </form>
+    </div>
+</div>
 
-<section>
+<div class="container d-flex justify-content-center">
+<div class="card mt-5 mb-5" style="width:50%;">
     <?php
     $getmsg=mysqli_query($db_handle, "SELECT * FROM echange WHERE dest=$e AND emet=$d OR dest=$d AND emet=$e");
     while ($data = mysqli_fetch_assoc($getmsg)) {
         if($data['dest'] == $d)
         {
-            ?><p style="color:red;"><?php echo $data['sms']; ?></p>
+            ?><p class="px-3 text-left" style="color:red;"><?php echo $data['sms']; ?></p>
             <?php   
         }
         else if($data['dest'] == $e)
         {
-            ?><p style="color:blue;"><?php echo $data['sms']; ?></p>
+            ?><p class="px-3 text-right" style="color:blue;"><?php echo $data['sms']; ?></p>
             <?php 
         }
     }
     //fermer la connexion
     mysqli_close($db_handle);
     ?>
-</section>
+</div>
+</div>
 </body>
 </html>
