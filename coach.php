015c3e3b5ecfec95ce5d7decb97c1496fd462288
louisthms <78725905+louisthms@users.noreply.github.com>
2022-05-27 10:25:19 +0200

    update coach consultation

--- coach.php
+++ coach.php
@@ -26,10 +26,12 @@ $prenom = isset($_POST["prenom"])? $_POST["prenom"] : "";
 $mail = isset($_POST["courrier"])? $_POST["courrier"] : "";
 $nomClient=0;
 $prenomClient="";
+$e=0;
 if (isset($_SESSION['cID'])) {
     $nom=$_SESSION['Nom'];
     $prenom=$_SESSION['Prenom'];
     $mail=$_SESSION['Mail'];
+    $e=$_SESSION['cID'];
 } 
 
 $erreur = "";
@@ -52,65 +54,6 @@ else
     //identifier votre serveur (localhost), utlisateur (root), mot de passe ("")
     $db_handle = mysqli_connect('localhost', 'root', '');
     $db_found = mysqli_select_db($db_handle, $database);
-    $res=mysqli_query($db_handle, "SELECT mID,Prenom FROM client");
-    ?>
-    <div class="container my-5 py-5">
-    <div class="border border-light py-5" style="border-radius: 33px;">
-    <h2 class="text-white text-center lead">Veuillez choisir avec qui vous voulez communiquer</h2>
-    <div class="row justify-content-center align-items-center">
-    <div class="col-lg-4 col-md-6 my-4 py-4 text-center">
-    <?php 
-    while ($data = mysqli_fetch_assoc($res)) {
-        $d=$data['mID'];
-        echo "<a class='btn btn-outline-light' href='communiquer.php?mID=$d'>" . $data['Prenom'] . "</a>";
-    }
-    ?>
-    <a class="btn btn-outline-light btn-lg mt-5" href="deconnexion.php">Deconnexion</a>
-    </div>
-    </div>
-    </div>
-    </div>
-    <div class="container my-5 py-5">
-    <div class="border border-light py-5" style="border-radius: 33px;">
-    <h2 class="text-white text-center lead">Consultations</h2>
-    <div class="row justify-content-center align-items-center">
-    <div class="col-lg-4 col-md-6 my-4 py-4 text-center">
-    <?php 
-    $sess=$_SESSION['cID'];
-    $r=mysqli_query($db_handle, "SELECT * FROM rdv WHERE c_id=$sess");
-    if (mysqli_num_rows($r)) {
-    echo "<table border=\"1\">";
-    echo "<tr>";
-    echo "<th>" . "Jour" . "</th>";
-    echo "<th>" . "Horaire" . "</th>";
-    echo "<th>" . "Client" . "</th>";
-    echo "</tr>";
-    while ($data = mysqli_fetch_assoc($r)) {
-        $nomClient=$data['clientID'];
-        $r1=mysqli_query($db_handle, "SELECT Prenom FROM client WHERE mID=$nomClient");
-        if (mysqli_num_rows($r)) {
-        while ($data1 = mysqli_fetch_assoc($r1)) {
-            $prenomClient=$data1['Prenom'];
-        }
-        echo "<tr>";
-        echo "<td>" . $data['jour'] . "</td>";
-        echo "<td>" . $data['horaire'] . "</td>";
-        echo "<td>" .  $prenomClient . "</td>";
-        echo "</tr>";
-        }
-    }
-    echo "</table>";
-    }
-    else
-    {
-        echo "Aucune consultation";
-    }
-    ?>
-    </div>
-    </div>
-    </div>
-    </div>
-    <?php 
     if (isset($_POST["coach"])) {
         if ($db_found) {
         $result =mysqli_query($db_handle, "SELECT * FROM coach WHERE Nom='$_POST[nom]' AND Prenom='$_POST[prenom]' AND Mail='$_POST[courrier]' ORDER BY cID ");
@@ -143,11 +86,67 @@ else
         else{
             echo "<br>Database not found";
         }
-        //fermer la connexion
-        mysqli_close($db_handle);
     }
-}  
-?>
+    $res=mysqli_query($db_handle, "SELECT mID,Prenom FROM client");
+    ?>
+    <div class="container my-5 py-5">
+    <div class="border border-light py-5" style="border-radius: 33px;">
+    <h2 class="text-white text-center lead">Veuillez choisir avec qui vous voulez communiquer</h2>
+    <div class="row justify-content-center align-items-center">
+    <div class="col-lg-4 col-md-6 my-4 py-4 text-center">
+    <?php 
+    while ($data = mysqli_fetch_assoc($res)) {
+        $d=$data['mID'];
+        echo "<a class='btn btn-outline-light' href='communiquer.php?mID=$d'>" . $data['Prenom'] . "</a>";
+    }
+    ?>
+    <a class="btn btn-outline-light btn-lg mt-5" href="deconnexion.php">Deconnexion</a>
+    </div>
+    </div>
+    </div>
+    </div>
+    <div class="container my-5 py-5">
+    <div class="border border-light py-5" style="border-radius: 33px;">
+    <h2 class="text-white text-center lead">Consultations</h2>
+    <div class="row justify-content-center align-items-center">
+    <div class="col-lg-4 col-md-6 my-4 py-4 text-center">
+    <?php 
+    $r=mysqli_query($db_handle, "SELECT * FROM rdv WHERE c_id=$e");
+    if (mysqli_num_rows($r)) {
+    echo "<table border=\"1\">";
+    echo "<tr>";
+    echo "<th>" . "Jour" . "</th>";
+    echo "<th>" . "Horaire" . "</th>";
+    echo "<th>" . "Client" . "</th>";
+    echo "</tr>";
+    while ($data = mysqli_fetch_assoc($r)) {
+        $nomClient=$data['clientID'];
+        $r1=mysqli_query($db_handle, "SELECT Prenom FROM client WHERE mID=$nomClient");
+        if (mysqli_num_rows($r)) {
+        while ($data1 = mysqli_fetch_assoc($r1)) {
+            $prenomClient=$data1['Prenom'];
+        }
+        echo "<tr>";
+        echo "<td>" . $data['jour'] . "</td>";
+        echo "<td>" . $data['horaire'] . "</td>";
+        echo "<td>" .  $prenomClient . "</td>";
+        echo "</tr>";
+        }
+    }
+    echo "</table>";
+    }
+    else
+    {
+        echo "Aucune consultation";
+    }
+    //fermer la connexion
+    mysqli_close($db_handle);
+}
+    ?>
+    </div>
+    </div>
+    </div>
+    </div>
 </body>
 </html>
  
