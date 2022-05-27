ed700e55caf2ba4f3b22817657cec81c3205e351
kleytar <btxkleytar1002@gmail.com>
2022-05-27 16:06:59 +0200

    update CV + delete and add a coach

--- adminTraitement.php
+++ adminTraitement.php
@@ -6,6 +6,8 @@ $file = isset($_POST["file"])? $_POST["file"] : "";
 $spe = isset($_POST["spe"])? $_POST["spe"] : "";
 $mail = isset($_POST["courrier"])? $_POST["courrier"] : "";
 $salle = isset($_POST["salle"])? $_POST["salle"] : "";
+$id=0;
+$pre="";
 
 $erreur = "";
 if ($nom == "") {
@@ -28,7 +30,7 @@ if ($salle == "") {
 }
 if ($erreur != "") {
     echo "Erreur: <br>" . $erreur;
-    header("Location: admin.php");
+    header("Location: choixAddCoach.php");
 }
 else
 {
@@ -40,8 +42,23 @@ if (isset($_POST["add"])) {
     if ($db_found) {
     $i=0;
     $sql="INSERT INTO coach(cID,Nom,Prenom,Photo,Specialite,Mail,Bureau)
-    VALUES($i,'$nom', '$prenom', '$file', '$spe', '$mail', '$salle')";
+    VALUES($i,'$nom', '$prenom', 'img/$file', '$spe', '$mail', '$salle')";
     $result =mysqli_query($db_handle, $sql);
+    
+    $get="SELECT cID,Prenom FROM coach WHERE (Nom='$_POST[nom]' AND Prenom='$_POST[prenom]' AND Photo='img/$_POST[file]' AND Specialite='$_POST[spe]' AND Mail='$_POST[courrier]' AND Bureau='$_POST[salle]')";
+    $r=mysqli_query($db_handle, $get);
+    while ($data = mysqli_fetch_assoc($r)) {
+    $id=$data['cID'];
+    $pre=$data['Prenom'];
+    $sql1="INSERT INTO cv(cvID,prenom,formation,experience)
+    VALUES($id,'$pre','Prof de sport','BAC S')";
+    $result1 =mysqli_query($db_handle, $sql1);
+
+    $sql2 = "INSERT INTO emploistemps(c_id,lundi_AM,lundi_PM,mardi_AM,mardi_PM,mercredi_AM,mercredi_PM,jeudi_AM,jeudi_PM,vendredi_AM,vendredi_PM,samedi_AM,samediPM)
+    VALUES($id,1,0,1,0,1,0,1,0,1,0,1,0)";
+    $result2 = mysqli_query($db_handle, $sql2);
+    header("Location: choixCV.php");
+    }
     }
     else{
         echo "<br>Database not found";
@@ -55,7 +72,7 @@ if (isset($_POST["delete"])) {
     $db_handle = mysqli_connect('localhost', 'root', '');
     $db_found = mysqli_select_db($db_handle, $database);
     if ($db_found) {
-    $getID="SELECT cID FROM coach WHERE (Nom='$_POST[nom]' AND Prenom='$_POST[prenom]' AND Photo='$_POST[file]' AND Specialite='$_POST[spe]' AND Mail='$_POST[courrier]' AND Bureau='$_POST[salle]')";
+    $getID="SELECT cID FROM coach WHERE (Nom='$_POST[nom]' AND Prenom='$_POST[prenom]' AND Photo='img/$_POST[file]' AND Specialite='$_POST[spe]' AND Mail='$_POST[courrier]' AND Bureau='$_POST[salle]')";
     $r=mysqli_query($db_handle, $getID);
     if (mysqli_num_rows($r)) {
     while ($data = mysqli_fetch_assoc($r)) {
@@ -63,6 +80,7 @@ if (isset($_POST["delete"])) {
     }
     $sql="DELETE FROM coach WHERE cID=$e";
     $result=mysqli_query($db_handle, $sql);
+    header("Location: admin.php");
     }
     else
     {
--- creationCV.php
+++ creationCV.php
@@ -18,7 +18,7 @@ if ($experience == "") {
 }
 if ($erreur != "") {
     echo "Erreur: <br>" . $erreur;
-    header("Location: admin.php");
+    header("Location: choixCV.php");
 }
 else
 {
@@ -51,10 +51,58 @@ if (isset($_POST["cv"])) {
             VALUES($idCoach,'$prenomCoach','$formation','$experience')";
             $result =mysqli_query($db_handle, $sql);
         }
+        $r=mysqli_query($db_handle, "SELECT * FROM cv WHERE cvID=$idCoach");
+        $document = new DOMDocument( "1.0", "UTF-8" );
+        $document->preserveWhiteSpace = false;
+        $document->formatOutput = true;
+
+        $xslt = $document->createProcessingInstruction('xml-stylesheet', 'type="text/css" href="css/xml.css"');
+        $document->appendChild($xslt);
+
+        /*
+        $html = $document->createElement('html');
+        $document->appendChild($html);
+
+        $body = $document->createElement('body');
+        $html->appendChild($body);
+        */
+        // on cree la racine et on l insère dans le document
+        $div = $document->createElement('CVs');
+        //$body->appendChild($div); 
+        $document->appendChild($div); 
+
+        if(mysqli_num_rows($r))
+        {
+            while ($data = mysqli_fetch_assoc($r)) {
+                $cv=$document->createElement('cv');
+                $div->appendChild($cv);
+                $prenomCV=$document->createElement('prenom');
+                $i1=$document->createTextNode($data['prenom']);
+                $prenomCV->appendChild($i1);
+                $cv->appendChild($prenomCV);
+
+                $formationCV=$document->createElement('formation');
+                $i2=$document->createTextNode($data['formation']);
+                $formationCV->appendChild($i2);
+                $cv->appendChild($formationCV);
+
+                $experienceCV=$document->createElement('experience');
+                $i3=$document->createTextNode($data['experience']);
+                $experienceCV->appendChild($i3);
+                $cv->appendChild($experienceCV);
+            }
+            //$document->appendChild($html);
+            $document->save("xml/$prenomCoach.xml");
+            header("Location:admin.php");
+        }
+        else
+        {
+            header("Location:choixCV.php");
+        }
     }
     else
     {
-        header("Location:admin.php");
+        header("Location:choixCV.php");
     }
     }
     else{
@@ -64,66 +112,4 @@ if (isset($_POST["cv"])) {
     mysqli_close($db_handle);
     } 
 }
-?>
-
-<?php 
-$database = "omnes";
-//identifier votre serveur (localhost), utlisateur (root), mot de passe ("")
-$db_handle = mysqli_connect('localhost', 'root', '');
-$db_found = mysqli_select_db($db_handle, $database);
-if ($db_found) {
-$r=mysqli_query($db_handle, "SELECT * FROM cv WHERE cvID=$idCoach");
-$document = new DOMDocument( "1.0", "UTF-8" );
-$document->preserveWhiteSpace = false;
-$document->formatOutput = true;
-
-$xslt = $document->createProcessingInstruction('xml-stylesheet', 'type="text/css" href="css/xml.css"');
-$document->appendChild($xslt);
-
-/*
-$html = $document->createElement('html');
-$document->appendChild($html);
-
-$body = $document->createElement('body');
-$html->appendChild($body);
-*/
-// on cree la racine et on l insère dans le document
-$div = $document->createElement('CVs');
-//$body->appendChild($div); 
-$document->appendChild($div); 
-
-if(mysqli_num_rows($r))
-{
-    while ($data = mysqli_fetch_assoc($r)) {
-        $cv=$document->createElement('cv');
-        $div->appendChild($cv);
-        $prenomCV=$document->createElement('prenom');
-        $i1=$document->createTextNode($data['prenom']);
-        $prenomCV->appendChild($i1);
-        $cv->appendChild($prenomCV);
-
-        $formationCV=$document->createElement('formation');
-        $i2=$document->createTextNode($data['formation']);
-        $formationCV->appendChild($i2);
-        $cv->appendChild($formationCV);
-
-        $experienceCV=$document->createElement('experience');
-        $i3=$document->createTextNode($data['experience']);
-        $experienceCV->appendChild($i3);
-        $cv->appendChild($experienceCV);
-    }
-    //$document->appendChild($html);
-    $document->save("xml/$prenomCoach.xml");
-    echo "Export XML fini !";
-}
-else
-{
-    header("Location:admin.php");
-}
-}
-else{
-    echo "<br>Database not found";
-}
-//fermer la connexion
-mysqli_close($db_handle);
 ?>
